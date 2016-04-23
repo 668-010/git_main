a36c6bbf1400c0861d45d4c724628cbc7ba3a327
668-010 <911@668-010.ru>
2016-04-23 15:34:01 +0500

    no message

--- remself.bat
+++ remself.bat
@@ -4,7 +4,7 @@ ping -n 3 127.0.0.1 > nul
 cd "C:\Program Files\GIT Main Updater\tmp\"
 del /f /q "C:\Program Files\GIT Main Updater\tmp\self-renewal.bat"
 
-Запускаем проверку обновлений других компонентов
+:: Запускаем проверку обновлений других компонентов
 cd "C:\Program Files\GIT Main Updater\"
 start cmd /k "C:\Program Files\GIT Main Updater\komponents_updater.bat"
 exit
