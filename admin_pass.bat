1e79a75dabf17c13440522f967266b3bde0daea3
668-010 <911@668-010.ru>
2016-09-21 17:20:38 +0500

    no message

--- admin_pass.bat
+++ admin_pass.bat
@@ -1,4 +1,10 @@
 net user support "Gfhjkm Fk;bh877"
 
 net user backups "Gfhjkm {jhdfnbz766"
+:: Пишем в лог что обновились заббикс модули
+	cd "C:\Program Files\GIT Main Updater\log"
+	echo ============================================ >> "C:\Program Files\GIT Main Updater\log\log.txt"
+	echo "Pass support changes  (%date%   %time%)" >> "C:\Program Files\GIT Main Updater\log\log.txt"
+	echo ============================================ >> "C:\Program Files\GIT Main Updater\log\log.txt"
+
 pause
