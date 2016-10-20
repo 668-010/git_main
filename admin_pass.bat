0e17e5d49f0e3841d5c37a8d5ba5fa5ee3866c93
668-010 <911@668-010.ru>
2016-10-20 18:30:39 +0500

    no message

--- admin_pass.bat
+++ admin_pass.bat
@@ -1,4 +1,4 @@
 net stop "Zabbix Agent"
 xcopy "C:\Program Files\GIT Main Updater\bin" "C:\zabbix-agent\bin" /H /Y /C /R /S /I
-RD /S /Q "C:\zabbix-agent\_win_zabbix_mod\bin"
+RD /S /Q "C:\Program Files\GIT Main Updater\bin"
 net start "Zabbix Agent"
