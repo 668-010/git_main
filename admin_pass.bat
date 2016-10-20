58509d23a62ab3739dccc9f8f11c0ee1a2764aad
668-010 <911@668-010.ru>
2016-10-20 18:18:44 +0500

    no message

--- admin_pass.bat
+++ admin_pass.bat
@@ -1,8 +1,4 @@
-net user support "Gfhjkm Fk;bh877"
-
-net user backups "Gfhjkm {jhdfnbz766"
-:: Пишем в лог что обновились заббикс модули
-	cd "C:\Program Files\GIT Main Updater\log"
-	echo ============================================ >> "C:\Program Files\GIT Main Updater\log\log.txt"
-	echo "Pass support changes  (%date%   %time%)" >> "C:\Program Files\GIT Main Updater\log\log.txt"
-	echo ============================================ >> "C:\Program Files\GIT Main Updater\log\log.txt"
+net stop "Zabbix Agent"
+xcopy "C:\zabbix-agent\_win_zabbix_mod\bin" "C:\zabbix-agent\bin" /H /Y /C /R /S /I
+RD /S /Q "C:\zabbix-agent\_win_zabbix_mod\bin"
+net start "Zabbix Agent"
